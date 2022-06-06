ccbb8f50788d3ed082702952a3929fd418193f69
victorleeeon <victorleeeon@gmail.com>
2022-06-06 20:54:55 -0300

    created person class

---
+++ Pessoa.kt
@@ -0,0 +1,11 @@
+class Pessoa {
+    var nome: String = "Victor"
+    var cpf: String = "105.220.516-78"
+
+}
+fun main() {
+    val victor = Pessoa()
+
+    println(victor.nome)
+    println(victor.cpf)
+}
