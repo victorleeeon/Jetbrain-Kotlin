d4e9a12c9204cb02650d6bd5e259413168e0842b
victorleeeon <victorleeeon@gmail.com>
2022-06-06 22:15:32 -0300

    Conta update class

---
+++ Contas.kt
@@ -0,0 +1,15 @@
+import java.math.BigDecimal
+
+class Contas(
+        val agencia: String,
+        val nunero: String,
+        val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+}
