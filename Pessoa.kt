c0768bb2a48ef4bce50697cb7cddc6ffa29549b6
victorleeeon <victorleeeon@gmail.com>
2022-06-06 21:24:52 -0300

    updated to private set fuction

--- Pessoa.kt
+++ Pessoa.kt
@@ -2,10 +2,19 @@ class Pessoa {
     var nome: String = "Victor"
     var cpf: String = "105.220.516-78"
 
+    inner class Endereco {
+        var rua: String = "Rua Teste"
+        private set
+
+    }
 }
 fun main() {
     val victor = Pessoa()
 
     println(victor.nome)
     println(victor.cpf)
+    println(victor.Endereco().rua)
+
 }
+
+
